0ae7f5f221082b4a039a0b337ab8962ad9d2e434
rmashhadi <rme1383@gmail.com>
2020-01-28 15:54:24 +0330

    Updates

---
+++ precache-manifest.8d0a352820f283f48dcad58d5bfb41c9.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "852aa672691ce3c7af6456d43e20a05c",
+    "url": "/Women-s-beauty/index.html"
+  },
+  {
+    "revision": "6a2063125b95310de621",
+    "url": "/Women-s-beauty/static/css/main.993455d3.chunk.css"
+  },
+  {
+    "revision": "8ce5990ddd58620b5a32",
+    "url": "/Women-s-beauty/static/js/2.565c81ec.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/Women-s-beauty/static/js/2.565c81ec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6a2063125b95310de621",
+    "url": "/Women-s-beauty/static/js/main.607de0d0.chunk.js"
+  },
+  {
+    "revision": "0ee02334566a3a3a46b9",
+    "url": "/Women-s-beauty/static/js/runtime-main.c5069289.js"
+  },
+  {
+    "revision": "c3aca4c1dd91f52e5b7c6b522e9c581d",
+    "url": "/Women-s-beauty/static/media/Women.c3aca4c1.png"
+  },
+  {
+    "revision": "a776baf1ea3e1c4d6482bc1ff8a0f01c",
+    "url": "/Women-s-beauty/static/media/fit.a776baf1.jpg"
+  },
+  {
+    "revision": "9ee1e8e0ff1c9dca8a51a32f807c250f",
+    "url": "/Women-s-beauty/static/media/keira-knightley.9ee1e8e0.png"
+  },
+  {
+    "revision": "706e84c76f84e2671f1ecb2ca29afcd3",
+    "url": "/Women-s-beauty/static/media/p1.706e84c7.png"
+  },
+  {
+    "revision": "047c36cbb391074aad5252de630c8d86",
+    "url": "/Women-s-beauty/static/media/p2.047c36cb.png"
+  },
+  {
+    "revision": "6ad0a400e98fbd4bd3ff6329754f0fc9",
+    "url": "/Women-s-beauty/static/media/p3.6ad0a400.png"
+  },
+  {
+    "revision": "12de1eb1c760c2b3e6dce157bdc3972f",
+    "url": "/Women-s-beauty/static/media/p4.12de1eb1.png"
+  }
+]);
